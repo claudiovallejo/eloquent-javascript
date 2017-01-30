6db6dfc6de7e53e838aab971b052244d8fda6794
Claudio Vallejo <hola@claudiovallejo.mx>
2017-01-30 17:23:13 -0600

    Finished Chapter 1 exercises.

--- part-1/chapter-1/exercises.js
+++ part-1/chapter-1/exercises.js
@@ -1,10 +1,10 @@
 //  Exercise #1
 var $js1 = document.getElementsByClassName('js-1')[0];
-var hash = "Δ";
+var hash = "🔺";
 for (var i = 0; i < 7; i++) {
-  var $div = "<p>" + hash + "</p>";
+  var $div = "<p style=\"margin: 0; padding: 0; line-height: 1rem;\">" + hash + "</p>";
   $js1.innerHTML += $div;
-  hash += "Δ";
+  hash += "🔺";
 }
 //  Exercise #2
 var $js2 = document.getElementsByClassName('js-2')[0];
@@ -20,3 +20,22 @@ for (var i = 1; i <= 100; i++) {
   print += " ";
 }
 $js2.innerHTML += print;
+//  Exercise #3
+var result = "";
+var size = 8;
+var symbolA = "◻️"
+var symbolB = "◼️"
+var $js3 = document.getElementsByClassName('js-3')[0];
+for (var i = 0; i < size; i++) {
+  var string = "<p style=\"margin: 0; padding: 0; line-height: 1rem;\">";
+  for (var e = 0; e < size; e++) {
+    if ((i + e) % 2 === 0) {
+      string += symbolA;
+    } else {
+      string += symbolB;
+    }
+  }
+  string += "</p>";
+  result += string;
+}
+$js3.innerHTML += result;
