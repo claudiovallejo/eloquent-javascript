c4018721d6f914d1b78eb67ff1f73d5aa2b9a285
Claudio Vallejo <hola@claudiovallejo.mx>
2017-02-27 23:10:33 -0600

    Started working on chapter 4 exercises.

--- part-1/chapter-4/c4-exercises.js
+++ part-1/chapter-4/c4-exercises.js
@@ -28,3 +28,43 @@ function range(start, end, step = 1) {
   }
   console.log(list);
 }
+//  Exercise #2: Reversing and Array
+function reverseArray(array) {
+  var newArray = [];
+  var n = array.length - 1;
+  for (n; n >= 0; n--) {
+    newArray.push(array[n]);
+  }
+  console.log(newArray);
+}
+
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var a = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = a;
+  }
+  console.log(array)
+}
+//  Exercise #3: A List
+function arrayToList(array) {
+  var list = null;
+  for (var i = array[array.length - 1]; i >= 0; i--) {
+    list = { value: array[i], rest: list };
+  }
+  console.log(list);
+}
+
+function listToArray(list) {
+  var array = [];
+  while (list !== null) {
+    array.push(list.value);
+    list = list.rest;
+  }
+  console.log(array);
+}
+
+function prepend(element, list) {
+  var newList = { value: element, rest: list };
+  console.log(newList);
+}
